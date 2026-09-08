5feebe472deda3f89ec22a9b3a3dee637b199883
240081601038 <240081601038@crescent.education>
2026-09-08 14:34:32 +0530

    play.py

--- app/youtube/play.py
+++ app/youtube/play.py
@@ -38,3 +38,22 @@ for pattern in patterns:
   pattern,
   test
 )
+query = command
+     for 
+     pattern in patterns:
+       match=re.search(
+         pattern,
+         text
+       )
+       if match:
+         query = match.group(1)
+         break
+  query = query.strip()
+video_id = get_vid(query)
+if not video_id:
+  return none
+return(
+  "https://www.youtube.com/embed/"
+  +video_id
+  +"?autoplay=1&mute=0"
+)
