028af2539bb5c8c9393bc9d382973080442b1857
240081601038 <240081601038@crescent.education>
2026-09-10 15:53:31 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,80 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, request
+from flask_cors import CORS
+
+from app.gmail import(
+    is_email_command,
+    extract_email,
+    create_gmail_url,
+    generate_email_with_gemini
+)
+
 from app.youtube import youtube_bp
 
-
-def create_app():
-
-    app = Flask(__name__)
-
+def create app():
+    app=Flask(__name__)
+    CORS(app)
+    
+   #YOUTUBE
     app.register_blueprint(
-        youtube_bp,
-        url_prefix="/youtube"
+    youtube bp,
+    url_prefix="/youtube"
     )
-
+    #HOME
     @app.route("/")
-    def home():
+    def home()
         return render_template("index.html")
-
+     #HTML
     @app.route("/html")
     def html():
         return render_template("index.html")
+    #Health
+    @app.route("/health")
+    def health():
+        return jsonify({
+            "status":"ok",
+            "service":"Nova AI Agent"
+        })
+        #Gmail AI Agent
+        @app.route(/"agent",methods=["POST"])
+        def agent():
+            try:
+                data = request.get_json(silent=True) or {}
+                command = data.get("command","").strip()
 
-    return app
+                if not command:
+                    return jsonify{(
+                    "success":False,
+                    "message": "Please give a Gmail command."
+                    }),400
+                if not is_email_command(command):
+                    return jsonify({
+                        "success":False,
+                        "message":"Please give a Gmail command."
+                    }),400
+               recipient = extract_email(command)
+            
+               email=generate_email_with_gemini(command)
+            
+               return jsonify({
+                        "success":True,
+                        "type": "email",
+                        "email_generated":True,
+                        "recipient":recipient,
+                        "subject":email["subject"],
+                        "body":email["body"],
+                        "gmail_url":create_gmail_url(
+                            email["subject"],
+                            email["body"],
+                            recipient
+                            }
+                              })
+                 
+                   except Exception as e:
+
+                       return jsontify({
+                           "success":False,
+                           "message":str(e)
+                       }),500
+                   return app                    
